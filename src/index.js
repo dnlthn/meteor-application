fe8b0e6d8da233b9d98bf945e6f65e091c4f582e
Daniel Thompson <dthompson844@gmail.com>
2018-03-06 17:21:58 -0600

    Added sanitize.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'sanitize.css';
+import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api.graph.cool/simple/v1/cjeerjj3914ra01934u1twzwb'
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
