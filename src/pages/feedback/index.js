4c56c90f9fe4f0a4f6dc774c6bf1cf1e090c0934
Daniel Thompson <dthompson844@gmail.com>
2018-03-07 09:49:54 -0600

    Started Feedback page

--- src/pages/feedback/index.js
+++ src/pages/feedback/index.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import glamorous from 'glamorous';
+import { Consumer as NavigationConsumer } from '../../context/navigation';
 
-const Feedback = () => {
-  return <div>Feedback Page</div>;
+import { BodyText } from '../../components/shared';
+import PrimaryButton from '../../components/primary-button';
+import TextButton from '../../components/text-button';
+
+const Container = glamorous.div({
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column'
+});
+
+const Title = glamorous.div({
+  fontSize: '1.875rem',
+  color: '#5661B3'
+});
+
+const ButtonContainer = glamorous.div({
+  display: 'flex',
+  justifyContent: 'center',
+  margin: '1.5rem'
+});
+
+const MarginBetweenSections = {
+  marginTop: '1.5rem'
 };
+
+const Feedback = () => (
+  <Container>
+    <div>
+      <Title>Feedback Welcome</Title>
+      <BodyText>
+        I encourage all types of feedback, regardless of whether it is negative
+        or positive. There are three ways to choose to give feedback.
+        <br />
+        <br />
+        If you would like a response from me, Slack will give the quickest
+        response time, though I will do my best to respond to an email as soon
+        as I see it.
+        <br />
+        <br />
+        If you do not want, or need, a response, feel free to leave a comment
+        below. You can choose to leave your name or comment anonymously.
+        Sometimes people do not want negativity attached to their name, but I
+        would still appreciate the negative feedback to identify areas for
+        improvement. The only thing I ask, is that you do not maliciously hit
+        the server with comments since there is not a requirement to login due
+        to the nature of this website. Additionally, this requirement means
+        comments are immutable once submitted.
+        <br />
+        <br />
+        I appreciate you taking the time to read this application and look
+        forward to any feedback.
+        <br />
+        <br />
+        Thanks,
+        <br />
+        Daniel Thompson
+      </BodyText>
+    </div>
+
+    <ButtonContainer>
+      <NavigationConsumer>
+        {({ navigate_to }) => (
+          <Fragment>
+            <TextButton onClick={navigate_to.resume}>SEND EMAIL</TextButton>
+            <PrimaryButton onClick={navigate_to.resume}>
+              VIEW RESUME
+            </PrimaryButton>
+          </Fragment>
+        )}
+      </NavigationConsumer>
+    </ButtonContainer>
+  </Container>
+);
 
 export default Feedback;
