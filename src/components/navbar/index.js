de66d1f9afb673f8f6dedbda327a250b71e99a59
Daniel Thompson <dthompson844@gmail.com>
2018-03-06 18:45:46 -0600

    Removed the ul from navbar to make it more flexible

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import glamorous from 'glamorous';
 import { Consumer as NavigationConsumer } from '../../context/navigation';
 
-const Nav = glamorous.ul({
-  listStyle: 'none'
-});
-
 const NavButton = glamorous.button(
   {
     padding: '.25rem .5rem .25rem .5rem',
@@ -33,23 +29,19 @@ const NavButton = glamorous.button(
 );
 
 const NavBar = () => (
-  <Nav>
-    <li>
-      <NavigationConsumer>
-        {({ current_page_name, navigate_to }) =>
-          Object.keys(navigate_to).map(page_name => (
-            <NavButton
-              key={page_name}
-              onClick={navigate_to[page_name]}
-              selected={current_page_name === page_name}
-            >
-              {page_name.toUpperCase()}
-            </NavButton>
-          ))
-        }
-      </NavigationConsumer>
-    </li>
-  </Nav>
+  <NavigationConsumer>
+    {({ current_page_name, navigate_to }) =>
+      Object.keys(navigate_to).map(page_name => (
+        <NavButton
+          key={page_name}
+          onClick={navigate_to[page_name]}
+          selected={current_page_name === page_name}
+        >
+          {page_name.toUpperCase()}
+        </NavButton>
+      ))
+    }
+  </NavigationConsumer>
 );
 
 export default NavBar;
